7f146e97aa88b0284a359139111b93b68f7313ef
David Knight <dave@mostlyspam.com>
2021-09-17 02:21:36 -0700

    Prevent Duplicate Page Guids

--- DWK_II/TimerToy_tt/Stored Procedures/InsertNewPage.sql
+++ DWK_II/TimerToy_tt/Stored Procedures/InsertNewPage.sql
@@ -4,6 +4,11 @@ AS
 
 declare  @PageKey char(36) = convert(char(36),newid());
 
+while exists ( select 1 from tt.[Page] p where left(p.PageKey, 28) = left(@PageKey, 28) )
+begin
+	select @PageKey = convert(char(36),newid());
+end
+
 insert into tt.Page(PageKey,PageName)
 select @PageKey, 'Bookmark this Page - Click To Edit'
 ;
