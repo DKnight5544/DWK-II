ebf540c88bce2263246b7c3ccab2054cdce9f0f9
David Knight <dave@mostlyspam.com>
2021-09-27 11:20:36 -0700

    Add Select All View

---
+++ DWK_II/TimerToy_tt2/Views/SelectAllView.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [tt2].[SelectAllView] AS 
+
+select
+	  pc.ParentKey
+	, t.TimerKey
+	, t.ReadOnlyKey
+	, t.TimerName
+	, t.CreationTime
+	, t.StartTime
+	, t.IsRunning
+	, ElapsedTime = tt2.GetElapsedTime(t.TimerKey)
+	, ChildCount = tt2.GetChildCount(t.TimerKey)
+
+from tt2.Timer t
+left join tt2.ParentChild pc 
+on pc.ChildKey = t.TimerKey
