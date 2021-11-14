c6f1448c49860da653820b2a1dbd41ab274b4151
DKnight5544 <dave@mostlyspam.com>
2021-11-14 11:08:02 -0800

    Update Sample Data Sproc for ORG.

--- DWK_II/ORG/Stored Procs/AddSampleData.sql
+++ DWK_II/ORG/Stored Procs/AddSampleData.sql
@@ -5,14 +5,11 @@ AS
 
 
 truncate table [org].[User]
-truncate table [org].[Trans]
 insert into org.[User] (UserName, SponsorName, OwnerName)
-select 'WIGIWIZ', 'WIGIWIZ','WIGIWIZ'
+select '@GoodKnightQuilts', '@GoodKnightQuilts','@GoodKnightQuilts'
 ;
 
 
-exec [org].[AddUser] 'WIGIWIZ', 'DOUGRALEY';
-exec [org].[AddUser] 'WIGIWIZ', 'JOLENEKNIGHT';
 
 
 
